3cf49e7f8e5273710d6b44b57833a503068656db
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2020-03-30 16:11:06 +0530

    Day 6 Assignment 2 added

--- Day6/is_prime_resolved.py
+++ Day6/is_prime_resolved.py
@@ -14,13 +14,3 @@ def is_prime(number):
     divisibility_list = list((number%x == 0) for x in range(2, int(number/2 if number/2 >= 2 else 2)))
     # print(divisibility_list)
     return bool(True not in divisibility_list)
-
-print(is_prime("0"))
-print(is_prime(1))
-print(is_prime(23))
-print(is_prime(77))
-print(is_prime(16))
-print(is_prime(98))
-print(is_prime(10005))
-print(is_prime(18745))
-print(is_prime(2))
